2b666f7303fadd2032c79890caa7d67237a07cfe
“ImogenHay” <imogen@hay.cx>
2020-11-17 16:37:53 +0000

    Added Homepage Tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
     assert_response :success
+
+    assert_select 'title', 'ImmyEvents'
+    assert_select 'h1', 'Welcome to ImmyEvents - Event Manager'
+    assert_select 'p', 'Find and Book Events Here!'
   end
 
 end
