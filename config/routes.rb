22965cd5ca8d3aa460896807d51a0d7866fe6b09
“ImogenHay” <imogen@hay.cx>
2020-11-19 22:15:34 +0000

    Location scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :locations
   root 'home#home' #sets home page/root to home/home
   get 'contact', to: 'home#contact' #root/contact url will bring to contact page (home controller, contact method)
   post 'request_contact', to: 'home#request_contact' #so submit button (contact us) will do request_contact action
---
+++ db/migrate/20201119221238_create_locations.rb
@@ -0,0 +1,13 @@
+class CreateLocations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :locations do |t|
+      t.string :name
+      t.text :description
+      t.string :country
+      t.string :street
+      t.string :postcode
+
+      t.timestamps
+    end
+  end
+end
