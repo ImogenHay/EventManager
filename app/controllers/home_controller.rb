6d41cc385128dfe72000a557d7ed2ec55ff5bb4d
“ImogenHay” <imogen@hay.cx>
2020-12-10 17:04:23 +0000

    Created About Us Page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,9 @@ class HomeController < ApplicationController
   def contact
   end
 
+  def about
+  end
+
   def request_contact #method called when submit button pressed
     name = params[:name]
     email = params[:email] #form values available via params hash
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users #resources for models
   resources :tickets
   resources :reviews
   resources :events
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   root 'home#home' #sets home page/root to home/home
   get 'contact', to: 'home#contact' #root/contact url will bring to contact page (home controller, contact method)
   post 'request_contact', to: 'home#request_contact' #so submit button (contact us) will do request_contact action
+  get 'about', to: 'home#about'  #root/about url will bring to about page (home controller, about method)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
