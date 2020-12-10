ec6a2dc46393e668aa480286876e4ca320bedbaa
“ImogenHay” <imogen@hay.cx>
2020-12-10 11:23:32 +0000

    Added dynamic navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,20 @@
 //= require turbolinks
 //= require_tree .
 
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("main_content").style.marginLeft = "250px";
+
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("main_content").style.marginLeft = "0";
+}
 
 function ToggleText(elem){
   $('#'+elem.id+"p").toggle();
